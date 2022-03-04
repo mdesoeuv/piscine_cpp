f45aa8b0abdf6e99534aeb50261229015b91e939
Mehdi <mehdi.desoeuvre@gmail.com>
2022-03-04 11:51:39 +0100

    cout style implementation

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,19 +6,41 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 11:20:40 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/04 11:42:12 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/04 11:50:42 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+void	capitalize(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	int	i;
+	
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	i = 1;
+	while (i < argc)
+	{
+		capitalize(argv[i]);
+		std::cout << argv[i];
+		i++;
+	}
+	std::cout << std::endl;
 	return (0);
 }
