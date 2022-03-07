a112400e12bd41cf0a643cc80e8a6ef155fd754a
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-03-07 08:32:30 +0100

    update

--- module_00/ex01/Contact.cpp
+++ module_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 08:48:59 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/05 09:10:38 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/05 18:52:37 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,12 @@ void	Contact::create(void)
 	std::cout << "<Enter Contact Info>" << std::endl;
 	std::cout << "First Name :";
 	std::getline(std::cin, this->first_name);
+	std::cout << "Last Name :";
+	std::getline(std::cin, this->last_name);
+	std::cout << "Nickname :";
+	std::getline(std::cin, this->nickname);
+	std::cout << "Phone number :";
+	std::getline(std::cin, this->phone_nbr);
+	std::cout << "Darkest secret :";
+	std::getline(std::cin, this->secret);
 }
