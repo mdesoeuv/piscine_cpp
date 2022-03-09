e97aba15c3b2f3aa03691e5e1ae036bd184ef405
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-03-09 16:03:26 +0100

    ex03 complete

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:36:35 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/09 15:49:48 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/09 16:03:03 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,12 @@ int	main(void)
 	std::string	*stringPTR = &str;
 	std::string	&stringREF = str;
 
-	std::cout << "real str address is : " << &(str[0]) << std::endl;
+	std::cout << "real str address is : " << &str << std::endl;
+	std::cout << "adress stored in stringPTR is : " << stringPTR << std::endl;
+	std::cout << "address stored in stringREF is : " << &stringREF << std::endl;
+	
+	std::cout << "str value is : " << str << std::endl;
+	std::cout << "value pointed by stringPTR is : " << *stringPTR << std::endl;
+	std::cout << "value pointed by stringREF is : " << stringREF << std::endl;
+	return (0);
 }
