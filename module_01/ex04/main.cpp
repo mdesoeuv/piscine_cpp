34cbe3cb3a585b1feb87bb8acee3752259396edf
Mehdi <mehdi.desoeuvre@gmail.com>
2022-03-10 13:49:11 +0100

    ex04 complete

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,15 +6,37 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 10:24:34 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/10 10:57:29 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/10 13:48:25 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-void	read_file(std::string)
+void	read_file(std::string& file_content, std::ifstream& input_file)
+{
+	std::string	line;
 
+	while (getline(input_file, line, '\n'))
+	{
+		file_content.append(line);
+		if (input_file.eof() == false)
+			file_content.append("\n");
+	}
+}
+
+void	replace_string(std::string& file_content, std::string string1, std::string string2)
+{
+	size_t	pos;
+
+	pos = file_content.find(string1, 0);
+	while (pos != file_content.npos)
+	{
+		file_content.erase(pos, string1.length());
+		file_content.insert(pos, string2);
+		pos = file_content.find(string1, 0);
+	}
+}
 
 int main(int argc, char **argv)
 {	
@@ -40,9 +62,13 @@ int main(int argc, char **argv)
 	if (!output_file)
 	{
 		std::cerr << "error: cannot create output file" << std::endl;
+		input_file.close();
 		return (EXIT_FAILURE);
 	}
-	read_file(file_content, input_file, output_file);
+	read_file(file_content, input_file);
+	if (string1.empty() == false)
+		replace_string(file_content, string1, string2);
+	output_file << file_content;
 	input_file.close();
 	output_file.close();
 	return (EXIT_SUCCESS);
