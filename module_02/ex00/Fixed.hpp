66c3009ab8d2e83558877bf0d26bfea1b12690da
Mehdi <mehdi.desoeuvre@gmail.com>
2022-03-11 17:34:54 +0100

    cleaning

--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 13:09:41 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/11 17:14:07 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/11 17:34:03 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class Fixed
 		Fixed(void);
 		Fixed(Fixed& fixed);
 		~Fixed(void);
-		Fixed	&operator=(const Fixed &);
+		Fixed	&operator=(const Fixed &source);
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 	
