2dd8e7c731ce0ce03ab806a556fb229ff48c1d30
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-03-24 15:21:18 +0100

    robotomisation working

--- module_05/ex02/RobotomyRequestForm.cpp
+++ module_05/ex02/RobotomyRequestForm.cpp
@@ -6,21 +6,24 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 11:08:44 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/24 15:06:03 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/24 15:19:41 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
 #include <iostream>
-#include <fstream>
+#include <ctime>
+#include <cstdlib>
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& source) : Form(source), target(source.target)
 {
+	srand(time(NULL));
 	std::cout << "RobotomyRequestForm copy constructor called" << std::endl;
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const std::string& Name, const std::string& Target) : Form(Name, 72, 45), target(Target)
 {
+	srand(time(NULL));
 	std::cout << "RobotomyRequestForm constructor called" << std::endl;
 }
 
@@ -31,6 +34,12 @@ RobotomyRequestForm::~RobotomyRequestForm(void)
 
 void	RobotomyRequestForm::specialFunction(void) const
 {
+	int result;
+
 	std::cout << "* Driller noises can be heard *" << std::endl;
-	
+	result = rand() % 2;
+	if (result)
+		std::cout << target << " has been robotomised" << std::endl;
+	else
+		std::cout << target << " resisted the robotomisation" << std::endl;
 }
--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -6,13 +6,14 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 08:56:57 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/24 12:06:48 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/24 15:20:36 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int	main(void)
 {
@@ -20,6 +21,7 @@ int	main(void)
 	Bureaucrat	JeanLouis("Jean-Louis", 150);
 
 	ShrubberyCreationForm		A("A493728B01", "Forest B12");
+	RobotomyRequestForm			R("R7294772", "Paul");
 
 	
 	std::cout << std::endl << Michel << JeanLouis << std::endl;
@@ -29,7 +31,21 @@ int	main(void)
 
 	std::cout << std::endl;
 
+	Michel.executeForm(R);
+	Michel.signForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
+	Michel.executeForm(R);
 	
+	std::cout << std::endl;
+	
+	JeanLouis.executeForm(R);
+
 	std::cout << std::endl;
 	
 	return (0);
