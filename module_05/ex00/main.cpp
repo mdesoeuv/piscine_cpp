b0fa9fc7a6225285bf83b96c1a4090affc70a738
Mehdi <mehdi.desoeuvre@gmail.com>
2022-03-29 11:48:34 +0200

    all exceptions catch in main demo

--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 08:56:57 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/03/29 11:42:17 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/03/29 11:47:52 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,42 @@ int	main(void)
 		Michel.upGrade();
 		std::cout << Michel << std::endl;
 		
+		std::cout << std::endl << Michel << JeanLouis << std::endl << std::endl;
+		std::cout << "trying to upgrade to grade 0" << std::endl;
 		Michel.upGrade();
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
 		std::cout << JeanLouis << std::endl << std::endl;
-		
+		std::cout << "trying to downgrade to 151" << std::endl;
 		JeanLouis.downGrade();
-
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << "trying to construct with grade -7" << std::endl;
 		Bureaucrat	Isidor("Isidor", -7);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::cout << "trying to construct with grade 151" << std::endl;
 		Bureaucrat	Isildur("Isildur", 151);
-
 	}
-	catch(std::exception& e)
+	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	catch(std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-
 	return (0);
 }
